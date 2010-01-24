eba99e6940438bd82f27467b228e9cf11ffcd331
Bram Westerbaan <bram@westerbaan.name>
2010-01-24 23:36:49 +0100

    IndentLexer: trailing dedent and new gens on iter

--- indent_lexer.py
+++ indent_lexer.py
@@ -1,24 +1,24 @@
 from itertools import takewhile
 
+__all__ = ['IndentLexer', 'VSpaceLexer']
 
 class IndentLexer:
 	def __init__(self, linesrc, indent_chars=' \t', 
-			indent_token='{', dedent_token='}'):
+			indent_token='{\n', dedent_token='}\n'):
 		self.linesrc = linesrc
 		self.indent_chars = indent_chars
 		self.indent_token = indent_token
 		self.dedent_token = dedent_token
 		self.indent_stack = []
-		self.generator = self._create_generator()
 	
 	def __iter__(self):
-		return self.generator
-
-	def _create_generator(self):
 		for line in self.linesrc:
 			for token in self._lex_line(line):
 				yield token
 			yield line
+		while len(self.indent_stack):
+			self.indent_stack.pop()
+			yield self.dedent_token
 
 	def _lex_line(self, line):
 		depth = 0
@@ -39,3 +39,8 @@ class IndentLexer:
 			return
 		self.indent_stack.append(new_indent)
 		yield self.indent_token
+
+
+
+
+
