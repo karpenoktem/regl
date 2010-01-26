3fef321ae238b9e6f43c113700df767681969099
Bram Westerbaan <bram@westerbaan.name>
2010-01-26 16:20:15 +0100

    conf

---
+++ conf.py
@@ -0,0 +1,34 @@
+from string import whitespace, ascii_letters, digits
+
+punctuation = ",.'\"/()-:;*?[]"
+specialChars = "#"
+indentTok = "^INDENT"
+dedentTok = "^DEDENT"
+vspaceTok = "^VSPACE"
+lineEndTok = "$"
+itemTok = "^ITEM"
+sectionSignTok = "<section-sign>"
+commentTok = "#"
+
+wordChars = ascii_letters + digits + punctuation
+allowedChars = wordChars + specialChars + whitespace
+
+charNames = (
+		(u"[", "[square-bracket-open]"),
+		(u"]", "[square-bracket-close]"),
+		(u"<", "[chevron-open]"),
+		(u">", "[chevron-close]"),
+		(u"\xe9", "[e-acute]"),
+		(u"\xeb", "[e-umlaut]"),
+		(u"\xa7", sectionSignTok),
+		(u"\u20ac", "[euro-sign]"))
+
+def createCharMap():
+	charMap = {}
+	for c in allowedChars:
+		charMap[c]=c
+	for c,n in charNames:
+		charMap[c]=n
+	return charMap
+charMap = createCharMap()  # can decorators do this?
+
