f70af57704966ace1f7ffdffabd501faa1772c44
Bram Westerbaan <bram@westerbaan.name>
2010-01-26 21:31:15 +0100

    regions_satisfying and string_regions_replace

--- aux.py
+++ aux.py
@@ -14,3 +14,21 @@ def compit(it):
 
 def dict_invert(d):
 	return dict([(v,k) for (k,v) in d.iteritems()])
+
+def regions_satisfying(pred, lst):
+	"""Returns the minimal amount of pairs (x_i,y_i) such that
+	pred(x) iff x in lst[x_i:y_i] for some i."""
+	epred = lambda i: (False if i in [-1,len(lst)] else pred(lst[i]))
+	switches = [i for i in xrange(0,len(lst)+1) if epred(i-1)^epred(i)]
+	assert len(switches) % 2 == 0
+	it = iter(switches)
+	for start in it:
+		stop = it.next()
+		yield (start,stop)
+
+def string_regions_replace(str, regions, replacement):
+	p = (0,)+reduce(lambda x,y: x+y, regions, ())+(len(str),)
+	return ''.join([str[p[i]:p[i+1]] if i%2==0 
+		else replacement(p[i],p[i+1]) 
+		for i in xrange(len(p)-1)])
+
