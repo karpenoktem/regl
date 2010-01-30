8d860f413966f4f6fca0f021e44091b72279d440
Bram Westerbaan <bram@westerbaan.name>
2010-01-30 18:35:52 +0100

    ItemLexer is not needed anymore

--- lexer.py
+++ lexer.py
@@ -182,6 +182,6 @@ class HSpaceLexer(Lexer):
 
 				
 def ReglLexer(linesrc):
-	return comp(LineEndLexer, ItemLexer, VSpaceLexer, IndentLexer, 
+	return comp(LineEndLexer, VSpaceLexer, IndentLexer, 
 			PyCommentLexer, HSpaceLexer, 
 			CharMapLexer)(linesrc, charMap)
