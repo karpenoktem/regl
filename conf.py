ea82465d05100c96d73c491af0930e1c1ce9f15c
Bram Westerbaan <bram@westerbaan.name>
2010-02-18 14:01:19 +0100

    some config changes
    Signed-off-by: Bram Westerbaan <bram@westerbaan.name>

--- conf.py
+++ conf.py
@@ -2,30 +2,30 @@ from string import whitespace, ascii_letters, digits
 
 from aux import dict_invert
 
-punctuation = ",.'\"/()-:;*?[]"
-specialChars = "#"
+punctuation = "!,.'\"\\/()-:;*?<>"
 tokTok = "^"
 indentTok = tokTok + "INDENT"
 dedentTok = tokTok + "DEDENT"
 vspaceTok = tokTok + "VSPACE"
-hspaceTok = "<HSPACE>"
+hspaceTok = "[HSPACE]"
 lineEndTok = "$"
 itemTok = tokTok + "ITEM"
-sectionSignTok = "<section-sign>"
+sectionSignTok = "[section-sign]"
+superTok = "~"
+parTok = "]"
 commentTok = "#"
+specialChars = commentTok + superTok + parTok
 
 wordChars = ascii_letters + digits + punctuation
 allowedChars = wordChars + specialChars + whitespace
 
 charNames = (
-		(u"[", "[square-bracket-open]"),
-		(u"]", "[square-bracket-close]"),
-		(u"<", "[chevron-open]"),
-		(u">", "[chevron-close]"),
-		(u"\xe9", "[e-acute]"),
-		(u"\xeb", "[e-umlaut]"),
+		(u"<", "<chevron-open>"),
+		(u">", "<chevron-close>"),
+		(u"\xe9", "<e-acute>"),
+		(u"\xeb", "<e-umlaut>"),
 		(u"\xa7", sectionSignTok),
-		(u"\u20ac", "[euro-sign]"))
+		(u"\u20ac", "<euro-sign>"))
 
 def createCharMap():
 	charMap = {}
